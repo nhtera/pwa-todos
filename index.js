f5059da72ea68954f780c0b36d4b8bfb4355f6fb
TienNH <nguyentien8x@gmail.com>
2017-05-04 07:26:55 +0700

    Serve nodejs

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 9000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+/* eslint-disable no-console */
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}/`);
+});
+/* eslint-enable no-console */
